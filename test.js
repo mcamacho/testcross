a39fde2f6a4c353acad63739892b13710cdeaa51
Mauricio Camacho <maurocamacho@gmail.com>
2018-03-12 16:32:36 -0400

    init

---
+++ test.js
@@ -0,0 +1,32 @@
+(function (dataLayer) {
+  var _config = {
+    roiqId: 'gQg95XW1',
+    fbId: '959501767509811',
+    phonenum: {
+      hardNumToFlip: '',
+      numToFlipCss: '.phone1 .value span'
+    },
+    chunk: {
+      notchunk: false,
+      clickUDClass: 'undefined',
+      clickUDType: 'submit',
+      formFrame: 'undefined'
+    },
+    form: {
+      search: '',
+      email: 'input[name="contact.email"]',
+      firstName: 'input[name="contact.firstName"]',
+      lastName: 'input[name="contact.lastName"]',
+      phone: 'input[name="contact.phone"]',
+      subject: 'input[name="form.id"]'
+    },
+    page: {
+      VDP: '\\/new|used|bargain|certified\\/.*',
+      SRP: '\\/(new|used|bargain|certified)-inventory\\/.*',
+      model: '\\/showroom'
+    },
+    vdpcustom: {}
+  }
+
+  dataLayer.push({event: 'configReady', config: _config})
+})(window.dataLayer)
